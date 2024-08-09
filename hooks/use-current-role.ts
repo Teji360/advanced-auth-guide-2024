cd435b2696122f847e0a56dbd6b7b9bc9399a539
Teji360 <TejiriD.Aror@gmail.com>
2024-08-09 03:08:15 -0400

    I just want to make some fucking changes bro

--- hooks/use-current-role.ts
+++ hooks/use-current-role.ts
@@ -2,7 +2,7 @@ import { useSession } from "next-auth/react";
 
 /**
  * 
- * Returns the currently logged in user's role.
+ * Returns the currently logged in user's role. I just want to make the fucking quota
  * @type {"ADMIN" | "USER"}
  */
 
